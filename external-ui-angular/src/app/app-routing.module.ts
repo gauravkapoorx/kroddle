9ba51ac45a6b994823d5f7229efeb6c310c9d0db
gaurav <gaurav@patientbond.com>
2023-09-25 09:57:53 +0530

    updated routing in angular

--- external-ui-angular/src/app/app-routing.module.ts
+++ external-ui-angular/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
 
-const routes: Routes = [];
+const routes: Routes = [ {
+  path: 'coming-soon',
+  component:ComingSoonComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { 
-  
+ 
 }
